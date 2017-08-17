bb532c30bc70a35b1595832ed92c4fbf998e922e
Mariano <mariano.eiberman@intive-fdv.com>
2017-08-17 17:55:43 -0300

    added the UI

--- src/ui/index.js
+++ src/ui/index.js
@@ -1,7 +1,13 @@
 import React from 'react';
 import { render } from 'react-dom';
+import Container from './container.js';
 
 class App extends React.Component {
+
+    componentDidCatch() {
+        return <div>OOOPSSS</div>;
+    }
+
     render() {
         return <div>Componente</div>;
     }
