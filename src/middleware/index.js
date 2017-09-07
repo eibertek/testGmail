28c1c9f2b61db9d27ca29cde76f53f60ab3f1841
Mariano <mariano.eiberman@intive-fdv.com>
2017-09-07 19:24:47 -0300

    add new data

--- src/middleware/index.js
+++ src/middleware/index.js
@@ -1,13 +1,34 @@
 import { Router } from 'express';
 import path from 'path';
 import { version } from '../../package.json';
-
+const sendMail =  require('gmail-send');
 export default ({ config }) => {
 	let routes = Router();
 
 	// add middleware here
   routes.get('/', (req, res) => {
 			res.sendFile(path.join(__dirname + './../views/index.html'));
+  });
+	
+		// add middleware here
+	routes.post('/send', (req, res) => {
+
+			// Override any default option and send email 
+			
+			console.log('* [example 1.1] sending test email');
+
+			var filepath = './demo-attachment.txt';  // File to attach 
+
+			sendMail({ // Overriding default parameters 
+				user: 'mariano.eiberman@intive-fdv.com',
+				pass: 'Agosto2017$',
+				to:   'mariano.eiberman@intive-fdv.com',
+				subject: 'attached ',         // Override value set as default 
+				//files: [ filepath ],
+				}, function (err, res) {
+					console.log('* [example 1.1] send() callback returned: err:', err, '; res:', res);
+					return res.json.send({'result':res});
+			});
   });
   
 	return routes;
