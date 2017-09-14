4abae9968ef63414e9cec420ce9a58eb0dfa2de5
eibertek <mariano.eiberman@gmail.com>
2017-09-14 11:51:31 -0300

    almost ready

--- src/ui/components/Employees/index.js
+++ src/ui/components/Employees/index.js
@@ -18,7 +18,72 @@ class Employees extends React.Component {
     }
 
     componentDidMount() {
-        if(this.props.employees.length === 0) this.props.load();
+        //Aqui agregar el fetch y luego dispatch la accion correspondiente
+        if(this.props.employees.length === 0) this.props.load([
+            {
+              "id": "meiberman05121984",
+              "name": "Mariano",
+              "lastname": "Eiberman",
+              "birthday": "05/12/1984",
+              "proyect": "Intelligize",
+              "startDay": "07/08/2017",
+              "picture": ""
+            },
+            {
+              "id": "meiberman045131",
+              "name": "Mariano",
+              "lastname": "Furriel",
+              "birthday": "05/11/1944",
+              "proyect": "Intelligize",
+              "startDay": "07/08/2017",
+              "picture": ""
+            },
+            {
+              "id": "meiberman04587184",
+              "name": "Daniel",
+              "lastname": "Mata",
+              "birthday": "05/12/1984",
+              "proyect": "Intelligize",
+              "startDay": "07/08/2017",
+              "picture": ""
+            },    
+            {
+              "id": "meiberman04587184",
+              "name": "Emanuel",
+              "lastname": "Suriano",
+              "birthday": "05/12/1984",
+              "proyect": "Intelligize",
+              "startDay": "07/08/2017",
+              "picture": ""
+            },        
+            {
+              "id": "meiberman04587184",
+              "name": "Marcos",
+              "lastname": "Marcos",
+              "birthday": "05/12/1984",
+              "proyect": "Intelligize",
+              "startDay": "07/08/2017",
+              "picture": ""
+            },        
+            {
+              "id": "meiberman04587184",
+              "name": "Maxi",
+              "lastname": "Cespedes",
+              "birthday": "05/12/1984",
+              "proyect": "Intelligize",
+              "startDay": "07/08/2017",
+              "picture": ""
+            },        
+            {
+              "id": "meiberman04587184",
+              "name": "Jose Daniel",
+              "lastname": "Sanchez",
+              "birthday": "05/12/1984",
+              "proyect": "Intelligize",
+              "startDay": "07/08/2017",
+              "picture": ""
+            },        
+          ]);
         var grid = new hypergrid('#grid1',  {
              data: this.props.employees
          })
